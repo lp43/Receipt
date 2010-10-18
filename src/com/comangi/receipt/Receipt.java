e9acc1d98c99c9920f910d192bce67ca7f75bd64
Simon <simon@camangi.com>
2010-10-18 18:07:41 +0800

    second commit

---
+++ src/com/comangi/receipt/Receipt.java
@@ -0,0 +1,13 @@
+package com.comangi.receipt;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Receipt extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
